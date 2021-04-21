eeb5033b6f49d65f5cdc0790d19855a4a1476706
hnimminh <hnimminh@outlook.com>
2021-04-21 18:51:40 +0700

    fix var type for switch

--- liberator/fsxmlapi.py
+++ liberator/fsxmlapi.py
@@ -8,7 +8,6 @@ from fastapi.templating import Jinja2Templates
 
 from configuration import (NODEID,
                            ESL_HOST, ESL_PORT, ESL_SECRET,
-                           MAX_SPS, MAX_SESSION, FIRST_RTP_PORT, LAST_RTP_PORT,
                            REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD, SCAN_COUNT)
 
 from utilities import logify, get_request_uuid, hashfieldify, jsonhash, getnameid, listify
@@ -28,8 +27,9 @@ templates = Jinja2Templates(directory="templates/fsxml")
 @fsxmlrouter.get("/fsxmlapi/switch", include_in_schema=False)
 def switch(request: Request, response: Response):
     try:
+        switchattributes = jsonhash(rdbconn.hgetall('cluster:attributes'))
         result = templates.TemplateResponse("switch.j2.xml",
-                                            {"request": request, "max_sessions": MAX_SESSION, "sessions_per_second": MAX_SPS, "rtp_start_port": FIRST_RTP_PORT, "rtp_end_port": LAST_RTP_PORT},
+                                            {"request": request, "switchattributes": switchattributes},
                                             media_type="application/xml")
         response.status_code = 200
     except Exception as e:
