a11f9f078d724517a14fee1171fb97e74ca49a4b
hnimminh <hnimminh@outlook.com>
2021-05-31 22:34:54 +0700

    typo fix

--- enginectl/event.unauth.lua
+++ enginectl/event.unauth.lua
@@ -5,7 +5,7 @@ local function unauth()
     local profilename = event:getHeader("profile-name")
     local user_agent = event:getHeader("user-agent")
     local network_ip = event:getHeader("network-ip")
-    logify('module', 'callflow', 'space', 'event:unauth', 'action', 'queue-block', 'profilename', profilename, 'user_agent', user_agent, 'network_ip', network_ip)
+    logify('module', 'enginectl', 'space', 'event:unauth', 'action', 'queue-block', 'profilename', profilename, 'user_agent', user_agent, 'network_ip', network_ip)
 end
 ---------------------******************************---------------------
 ---------------------*****|       MAIN       |*****---------------------
@@ -17,59 +17,59 @@ end
 ---- close log ----
 syslog.closelog()
 
---[[                                                                                                                                                                                                 
-Event-Subclass: sofia::pre_register                                                                                                                                                                           
-Event-Name: CUSTOM                                                                                                                                                                                            
-Core-UUID: 3f277dce-1172-4628-aa59-ec977cdf319b                                                                                                                                                               
-FreeSWITCH-Hostname: libresbc1                                                                                                                                                                                
-FreeSWITCH-Switchname: libresbc1                                                                                                                                                                              
-FreeSWITCH-IPv4: <LIBRESBC-IP>                                                                                                                                                                               
-FreeSWITCH-IPv6: ::1                                                                                                                                                                                          
-Event-Date-Local: 2021-05-03 05:20:40                                                                                                                                                                         
-Event-Date-GMT: Mon, 03 May 2021 05:20:40 GMT                                                                                                                                                                 
-Event-Date-Timestamp: 1620019240519134                                                                                                                                                                        
-Event-Calling-File: sofia_reg.c                                                                                                                                                                               
-Event-Calling-Function: sofia_reg_handle_register_token                                                                                                                                                       
-Event-Calling-Line-Number: 1763                                                                                                                                                                               
-Event-Sequence: 727                                                                                                                                                                                           
-profile-name: outside                                                                                                                                                                                         
-from-user: 33123456                                                                                                                                                                                           
-from-host: <LIBRESBC-IP>                                                                                                                                                                                     
-contact: "" <sip:3312345678@<FAREND-IP>:5060>                                                                                                                                                                
-call-id: 52d9fa751db23cb752fbe220226a41b6@<FAREND-IP>:5060                                                                                                                                                   
-rpid: unknown                                                                                                                                                                                                 
-status: Registered(UDP)                                                                                                                                                                                       
-expires: 300                                                                                                                                                                                                  
-to-user: 3312345678                                                                                                                                                                                           
-to-host: <FAREND-IP>                                                                                                                                                                                         
-network-ip: <FAREND-IP>                                                                                                                                                                                      
-network-port: 5060                                                                                                                                                                                            
+--[[
+Event-Subclass: sofia::pre_register
+Event-Name: CUSTOM
+Core-UUID: 3f277dce-1172-4628-aa59-ec977cdf319b
+FreeSWITCH-Hostname: libresbc1
+FreeSWITCH-Switchname: libresbc1
+FreeSWITCH-IPv4: <LIBRESBC-IP>
+FreeSWITCH-IPv6: ::1
+Event-Date-Local: 2021-05-03 05:20:40
+Event-Date-GMT: Mon, 03 May 2021 05:20:40 GMT
+Event-Date-Timestamp: 1620019240519134
+Event-Calling-File: sofia_reg.c
+Event-Calling-Function: sofia_reg_handle_register_token
+Event-Calling-Line-Number: 1763
+Event-Sequence: 727
+profile-name: outside
+from-user: 33123456
+from-host: <LIBRESBC-IP>
+contact: "" <sip:3312345678@<FAREND-IP>:5060>
+call-id: 52d9fa751db23cb752fbe220226a41b6@<FAREND-IP>:5060
+rpid: unknown
+status: Registered(UDP)
+expires: 300
+to-user: 3312345678
+to-host: <FAREND-IP>
+network-ip: <FAREND-IP>
+network-port: 5060
 user-agent: <FAREND-UA> 
-                                                                                                                                                                                                
-Event-Subclass: sofia::register_attempt                                                                                                                                                                       
-Event-Name: CUSTOM                                                                                                                                                                                            
-Core-UUID: 3f277dce-1172-4628-aa59-ec977cdf319b                                                                                                                                                               
-FreeSWITCH-Hostname: libresbc1                                                                                                                                                                                
-FreeSWITCH-Switchname: libresbc1                                                                                                                                                                              
-FreeSWITCH-IPv4: <LIBRESBC-IP>                                                                                                                                                                               
-FreeSWITCH-IPv6: ::1                                                                                                                                                                                          
-Event-Date-Local: 2021-05-03 05:20:40                                                                                                                                                                         
-Event-Date-GMT: Mon, 03 May 2021 05:20:40 GMT                                                                                                                                                                 
-Event-Date-Timestamp: 1620019240539062                                                                                                                                                                        
-Event-Calling-File: sofia_reg.c                                                                                                                                                                               
-Event-Calling-Function: sofia_reg_handle_register_token                                                                                                                                                       
-Event-Calling-Line-Number: 1558                                                                                                                                                                               
-Event-Sequence: 730                                                                                                                                                                                           
-profile-name: outside                                                                                                                                                                                         
-from-user: 33123456                                                                                                                                                                                           
-from-host: <LIBRESBC-IP>                                                                                                                                                                                     
-contact: "" <sip:3312345678@<FAREND-IP>:5060>                                                                                                                                                                
-call-id: 52d9fa751db23cb752fbe220226a41b6@<FAREND-IP>:5060                                                                                                                                                   
-rpid: unknown                                                                                                                                                                                                 
-status: Registered(UDP)                                                                                                                                                                                       
-expires: 300                                                                                                                                                                                                  
-to-user: 3312345678                                                                                                                                                                                           
-to-host: <FAREND-IP>                                                                                                                                                                                         
+
+Event-Subclass: sofia::register_attempt
+Event-Name: CUSTOM
+Core-UUID: 3f277dce-1172-4628-aa59-ec977cdf319b
+FreeSWITCH-Hostname: libresbc1
+FreeSWITCH-Switchname: libresbc1
+FreeSWITCH-IPv4: <LIBRESBC-IP>
+FreeSWITCH-IPv6: ::1
+Event-Date-Local: 2021-05-03 05:20:40
+Event-Date-GMT: Mon, 03 May 2021 05:20:40 GMT
+Event-Date-Timestamp: 1620019240539062
+Event-Calling-File: sofia_reg.c
+Event-Calling-Function: sofia_reg_handle_register_token
+Event-Calling-Line-Number: 1558
+Event-Sequence: 730
+profile-name: outside
+from-user: 33123456
+from-host: <LIBRESBC-IP>
+contact: "" <sip:3312345678@<FAREND-IP>:5060>
+call-id: 52d9fa751db23cb752fbe220226a41b6@<FAREND-IP>:5060
+rpid: unknown
+status: Registered(UDP)
+expires: 300
+to-user: 3312345678
+to-host: <FAREND-IP>
 network-ip: <FAREND-IP>
 network-port: 5060
 username: AWSFPBX
