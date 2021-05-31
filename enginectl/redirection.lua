aea9827c288b87fc4c0f8dfc68a4bc90f0ade27c
hnimminh <hnimminh@outlook.com>
2021-05-31 22:35:05 +0700

    log notice redirection

---
+++ enginectl/redirection.lua
@@ -0,0 +1,2 @@
+dofile("{{rundir}}/enginectl/configuration.lua")
+logify('module', 'enginectl', 'space', 'redirection', 'action', 'report', 'notice', 'please file a feature request for manual handle sip redirection')
