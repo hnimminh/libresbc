2ff4a32fbf3968323b83d4d456aaa479282f0e52
hnimminh <hnimminh@outlook.com>
2021-07-11 23:18:23 +0700

    v0.5.3

--- liberator/configuration.py
+++ liberator/configuration.py
@@ -11,7 +11,7 @@
 #      GLOBAL CONFIGURATION FILES
 #-----------------------------------------------------------------------------------------------------
 _APPLICATION = 'LIBRESBC'
-_SWVERSION = '0.5.3'
+_SWVERSION = '0.5.4'
 _DESCRIPTION = 'Open Source Session Border Controller for Large-Scale Voice Infrastructures'
 #-----------------------------------------------------------------------------------------------------
 # REDIS ENDPOINT
