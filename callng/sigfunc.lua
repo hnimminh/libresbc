07cd749938c9cb500eb0d3c063cc2cbf2739fae3
hnimminh <hnimminh@outlook.com>
2021-08-06 23:15:08 +0700

    define access function

--- callng/sigfunc.lua
+++ callng/sigfunc.lua
@@ -1,10 +1,24 @@
 --
 -- callng:sigfunc.lua
--- 
+--
 -- The Initial Developer of the Original Code is
 -- Minh Minh <hnimminh at[@] outlook dot[.] com>
--- Portions created by the Initial Developer are Copyright (C) the Initial Developer. 
+-- Portions created by the Initial Developer are Copyright (C) the Initial Developer.
 -- All Rights Reserved.
 -- ------------------------------------------------------------------------------------------------------------------------------------------------
 
 require("callng.utilities")
+
+
+function digestauth()
+end
+
+function ipauth()
+end
+
+function antiattack()
+end
+
+function bruteforce()
+end
+
