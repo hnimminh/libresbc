7d3ca425b17386845401ca2de2eac8354a4c77fc
hnimminh <hnimminh@outlook.com>
2021-08-16 00:00:08 +0700

    bugfix optional flooding

--- liberator/templates/kamcfg/layer.j2.lua
+++ liberator/templates/kamcfg/layer.j2.lua
@@ -1,12 +1,18 @@
 json = require('json')
 
-LAYER = '{{kamcfgs.name}}'
+LAYER = '{{layer}}'
 B2BUA_LOOPBACK_IPADDRS = {{swipaddrs}}
 DOMAIN_POLICIES = json.decode('{{jsonpolicies}}')
 
+{%- if kamcfgs.antiflooding %}
 AUTHFLOODING_THRESHOLD = {{kamcfgs.antiflooding.threshold}}
 AUTHFLOODING_BANTIME = {{kamcfgs.antiflooding.bantime}}
+{%- endif %}
 AUTHFAILURE_THRESHOLD = {{kamcfgs.authfailure.threshold}}
 AUTHFAILURE_BANTIME = {{kamcfgs.authfailure.bantime}}
 ATTACKAVOID_THRESHOLD = {{kamcfgs.attackavoid.threshold}}
 ATTACKAVOID_BANTIME = {{kamcfgs.attackavoid.bantime}}
+
+BRANCH_NATOUT_FLAG = {{_KAMCONST.BRANCH_NATOUT_FLAG}}
+BRANCH_NATSIPPING_FLAG = {{_KAMCONST.BRANCH_NATSIPPING_FLAG}}
+LIBRE_USER_LOCATION = '{{_KAMCONST.LIBRE_USER_LOCATION}}'
