22069b38e2911c9c15de5385acdb4128a51dd719
hnimminh <hnimminh@outlook.com>
2021-08-15 11:48:45 +0700

    spacing

--- callng/event.startup.lua
+++ callng/event.startup.lua
@@ -1,9 +1,9 @@
 --
 -- callng:event.startup.lua
--- 
+--
 -- The Initial Developer of the Original Code is
 -- Minh Minh <hnimminh at[@] outlook dot[.] com>
--- Portions created by the Initial Developer are Copyright (C) the Initial Developer. 
+-- Portions created by the Initial Developer are Copyright (C) the Initial Developer.
 -- All Rights Reserved.
 --
 
