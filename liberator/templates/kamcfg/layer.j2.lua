5bfa68be55fba1f42646e80421db573cf97ae319
hnimminh <hnimminh@outlook.com>
2021-08-15 20:56:40 +0700

    reconstant

--- liberator/templates/kamcfg/layer.j2.lua
+++ liberator/templates/kamcfg/layer.j2.lua
@@ -1,11 +1,12 @@
 json = require('json')
 
-NODEID = '{{nodeid}}'
-LAYER = '{{layer}}'
+LAYER = '{{kamcfgs.name}}'
 B2BUA_LOOPBACK_IPADDRS = {{swipaddrs}}
 DOMAIN_POLICIES = json.decode('{{jsonpolicies}}')
 
-AUTHFAILURE_THREDHOLD = 20
-AUTHFAILURE_WINDOW = 600
-BRUTEFORCE_THREDHOLD = 5
-BRUTEFORCE_WINDOW = 18000
+AUTHFLOODING_THRESHOLD = {{kamcfgs.antiflooding.threshold}}
+AUTHFLOODING_BANTIME = {{kamcfgs.antiflooding.bantime}}
+AUTHFAILURE_THRESHOLD = {{kamcfgs.authfailure.threshold}}
+AUTHFAILURE_BANTIME = {{kamcfgs.authfailure.bantime}}
+ATTACKAVOID_THRESHOLD = {{kamcfgs.attackavoid.threshold}}
+ATTACKAVOID_BANTIME = {{kamcfgs.attackavoid.bantime}}
