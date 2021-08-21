cc7f72e95546a31c0d2b6f48a6b74a93625144c2
hnimminh <hnimminh@outlook.com>
2021-08-21 23:24:41 +0700

    luarocks

---
+++ callng/requirement.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/usr/bin/luarocks install luaposix 35.0-1
+/usr/bin/luarocks install luaredis 2.0.5-2
+/usr/bin/luarocks install http 0.3-0
+/usr/bin/luarocks install luasocket 3.0rc1-2
+/usr/bin/luarocks install moonjson 0.1.2-1
