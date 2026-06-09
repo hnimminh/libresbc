41c8b831b7ca46566ab8a173de1e498d9f5246cb
brancan <nicobranca@gmail.com>
2026-06-09 15:42:12 -0300

    fix(callng): increase CDRTTL to 48h to prevent silent CDR loss on liberator downtime

--- callng/configuration.lua
+++ callng/configuration.lua
@@ -41,7 +41,7 @@ ROUTE = 'route'
 HTTPR = 'httpr'
 EMPTYSTRING = ''
 --- CDR
-CDRTTL = 3600
+CDRTTL = 172800  -- 48 hours; reduced from 1h to prevent silent CDR loss on liberator downtime
 
 --- SECURITY
 ROLLING_WINDOW_TIME = 1000                             --- use the exactly 1 second = 1000ms
